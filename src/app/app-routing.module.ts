610bdeb410384a93b114b88492fb928d28f82fae
MohamedAzizJnayah <mohamedazizjnayah@gmail.com>
2024-07-31 18:41:27 +0100

    Routing2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AjoutComponent } from './ajout/ajout.component';
 import { ListComponent } from './list/list.component';
+import { FirstComponent } from './first/first.component';
 
 const routes: Routes = [
   {path:'ajout' , component:AjoutComponent},
-  {path:'list' , component:ListComponent}
+  {path:'list' , component:ListComponent},
+  {path:'app' , component:FirstComponent}
 
 ];
 
